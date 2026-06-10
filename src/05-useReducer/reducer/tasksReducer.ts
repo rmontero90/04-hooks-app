8637f7b26201d3ceff7a1aac63bb8f10faa9060c
Rafael Montero <developer@rmontero.me>
2026-06-09 21:28:56 -0600

    Create tasksReducer.ts

---
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -0,0 +1,24 @@
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TaskState {
+  todos: Todo[];
+  length: number;
+  completed: number;
+  pending: number;
+}
+
+export type TaskAction =
+  | { type: "ADD_TODO"; payload: string }
+  | { type: "TOGGLE_TODO"; payload: number }
+  | { type: "DELETE_TODO"; payload: number };
+
+export const taskReducer = (
+  state: TaskState,
+  action: TaskAction,
+): TaskState => {
+  return state;
+};
