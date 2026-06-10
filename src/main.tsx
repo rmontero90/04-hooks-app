0f96da9f5e96365ee90476fa6ea1355db63f2903
Rafael Montero <developer@rmontero.me>
2026-06-09 20:18:51 -0600

    Add shadcn UI components and TaskApp
    Add shadcn components config and new UI primitives (button, card, checkbox, input) plus a TaskApp using useReducer and a small utils module.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
 // import { PokemonPage } from "./03-examples/PokemonPage";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+import { TasksApp } from "./05-useReducer/TaskApp";
 
 // import { HooksApp } from "./HooksApp";
 // import { TrafficLight } from "./01-useState/TrafficLight";
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 );
